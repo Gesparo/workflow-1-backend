e8bb89b92059e544af1931483e78e1ac6e3d6ce4
Melnik Maksim <shmaxer@rambler.ru>
2016-10-14 18:40:58 +0500

    Refactor: slider now in module, new type of switch imgs

--- source/js/app.js
+++ source/js/app.js
@@ -19,6 +19,87 @@ var blur = (function () {
   };
 })();
 
+var slider = (function () {
+  var nav = $('.site-slider__nav'),
+      prevNav = nav.find('.site-slider__nav-prev'),
+      nextNav = nav.find('.site-slider__nav-next'),
+      prevList = prevNav.find('.site-slider__nav-list'),
+      nextList = nextNav.find('.site-slider__nav-list');
+
+  var init = function () {
+    var prevItems = prevNav.find('.site-slider__nav-item'),
+        nextItems = nextNav.find('.site-slider__nav-item'),
+        activePrevItem = prevItems.filter('.site-slider__active-slide'),
+        activeNextItem = nextItems.filter('.site-slider__active-slide');
+
+    animateNavSlider(nextList, (activeNextItem.offset().top - nav.offset().top));
+    animateNavSlider(prevList, (activePrevItem.offset().top - nav.offset().top));
+
+    $(".site-slider").on('click', '.site-slider__nav-prev, .site-slider__nav-next', function () {
+      var $this = $(this),
+          prevItems = prevNav.find('.site-slider__nav-item'),
+          nextItems = nextNav.find('.site-slider__nav-item'),
+          activePrevItem = prevItems.filter('.site-slider__active-slide'),
+          activeNextItem = nextItems.filter('.site-slider__active-slide'),
+          prevBeforeItem = activePrevItem.next(),
+          prevNextItem = activePrevItem.prev(),
+          nextBeforeItem = activeNextItem.prev(),
+          nextNextItem = activeNextItem.next(),
+          navOffset = nav.offset().top,
+          posNextSlider = 0,
+          posPrevSlider = 0;
+
+      if( $this.hasClass('site-slider__nav-prev') ) {
+
+        if( !nextBeforeItem.length ) {
+          nextBeforeItem = nextItems.last();
+        }
+
+        if( !prevBeforeItem.length ) {
+          prevBeforeItem = prevItems.first();
+        }
+
+        posNextSlider = nextBeforeItem.offset().top - navOffset;
+        posPrevSlider = prevBeforeItem.offset().top - navOffset;
+
+        switchActiveClass(nextBeforeItem);
+        switchActiveClass(prevBeforeItem);
+
+      } else if ( $this.hasClass('site-slider__nav-next') ) {
+
+        if( !nextNextItem.length ) {
+          nextNextItem = nextItems.first();
+        }
+
+        if( !prevNextItem.length ) {
+          prevNextItem = prevItems.last();
+        }
+
+        posNextSlider =  nextNextItem.offset().top - navOffset;
+        posPrevSlider = prevNextItem.offset().top - navOffset;
+
+        switchActiveClass(nextNextItem);
+        switchActiveClass(prevNextItem);
+      }
+
+      animateNavSlider(nextList, posNextSlider);
+      animateNavSlider(prevList, posPrevSlider);
+    });
+  };
+
+  var animateNavSlider = function (slider, pos) {
+    slider.stop().animate({'top' : '-=' + pos + 'px'});
+  }
+
+  var switchActiveClass = function (slide) {
+    slide.addClass('site-slider__active-slide').siblings().removeClass('site-slider__active-slide');
+  };
+
+  return {
+    init : init
+  };
+})();
+
 $(window).on('resize', function () {
   blur.init();
 });
@@ -29,6 +110,9 @@ $(window).on('resize', function () {
   // Blur form
   blur.init();
 
+  // Slider init
+  slider.init();
+
   // scroll to page top
   $('.feedback__page-up').on('click', function () {
     $('html, body').animate({scrollTop: 0}, 300);
@@ -36,8 +120,49 @@ $(window).on('resize', function () {
 
 
 
-  $(".site-slider").on('click', '.site-slider__nav-prev', function () {
-    var $this = $(this),
+  /*$(".site-slider").on('click', '.site-slider__nav-prev, .site-slider__nav-next', function () {
+
+
+
+    if( $this.hasClass('site-slider__nav-prev') ) {
+
+      if( !nextBeforeItem.length ) {
+        nextBeforeItem = nextItems.last();
+      }
+
+      if( !prevBeforeItem.length ) {
+        prevBeforeItem = prevItems.last();
+      }
+
+      posPrevSlider = posNextSlider = nextBeforeItem.offset().top - navOffset;
+
+      switchActiveClass(nextBeforeItem);
+      switchActiveClass(prevBeforeItem);
+
+    } else if ( $this.hasClass('site-slider__nav-next') ) {
+
+      if( !nextNextItem.length ) {
+        nextNextItem = nextItems.first();
+      }
+
+      if( !prevNextItem.length ) {
+        prevNextItem = prevItems.first();
+      }
+
+      posPrevSlider = posNextSlider =  nextNextItem.offset().top - navOffset;
+
+      switchActiveClass(nextNextItem);
+      switchActiveClass(prevNextItem);
+    }
+
+    nextList.stop().animate({'top' : '-=' + posNextSlider + 'px'});
+    prevList.stop().animate({'top' : '-=' + posPrevSlider + 'px'});
+    
+    function switchActiveClass(slide) {
+      slide.addClass('site-slider__active-slide').siblings().removeClass('site-slider__active-slide');
+    }*/
+
+   /* var $this = $(this),
         main = $this.closest('.main-content'),
         sliderNav = $this.closest('.site-slider__nav'),
         prevNavImgArr = $this.find('.site-slider__nav-prev-img'),
@@ -54,16 +179,18 @@ $(window).on('resize', function () {
 
     prevNavImgArr.removeClass('site-slider_active-slide');
     nextNavImgArr.removeClass('site-slider_active-slide');
-    presentSiteImgArr.removeClass('site-slider_active-present');
+    // presentSiteImgArr.;
+    presentSiteImgArr.filter('.site-slider_active-present').removeClass('site-slider_active-present').fadeOut(300);
     siteIntroductionArr.removeClass('site-introduction_active');
 
     prevNavImgArr.eq(index).addClass('site-slider_active-slide');
     nextNavImgArr.eq(index).addClass('site-slider_active-slide');
-    presentSiteImgArr.eq(index).addClass('site-slider_active-present');
-    siteIntroductionArr.eq(index).addClass('site-introduction_active');
-  });
+    // presentSiteImgArr.eq(index).addClass('site-slider_active-present');
+    presentSiteImgArr.eq(index).fadeIn(300).addClass('site-slider_active-present');
+    siteIntroductionArr.eq(index).addClass('site-introduction_active');*/
+  /*});*/
 
-  $(".site-slider").on('click', '.site-slider__nav-next', function () {
+  /*$(".site-slider").on('click', '.site-slider__nav-next', function () {
     var $this = $(this),
         main = $this.closest('.main-content'),
         sliderNav = $this.closest('.site-slider__nav'),
@@ -88,6 +215,6 @@ $(window).on('resize', function () {
     nextNavImgArr.eq(index).addClass('site-slider_active-slide');
     presentSiteImgArr.eq(index).addClass('site-slider_active-present');
     siteIntroductionArr.eq(index).addClass('site-introduction_active');
-  });
+  });*/
 
 })();
