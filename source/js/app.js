b6cc413b6b78b5d3d9b419915f5201af04d35846
Melnik Maksim <shmaxer@rambler.ru>
2016-10-18 11:42:25 +0500

    Added parallax effect on inroduction page

--- source/js/app.js
+++ source/js/app.js
@@ -160,12 +160,52 @@ var slider = (function () {
   };
 })();
 
+var parallax = (function () {
+  var parallaxBlock = $('.parallax'),
+      layers = parallaxBlock.find('.parallax__layer');
+
+  var init = function () {
+    $(window).on('mousemove', function (e) {
+      var window = $(window),
+          mousepos_x = e.pageX,
+          mousepos_y = e.pageY,
+
+          windowWidth =  window.width(),
+          windowHeight = window.height(),
+          w = windowWidth / 2 - mousepos_x,
+          h = windowHeight / 2 - mousepos_y;
+
+
+
+      layers.map(function (key, value) {
+        var coefficient_w = (key + 1) / 100,
+            coefficient_h = (key + 1) / 100,
+            widthPos = w * coefficient_w,
+            heightPos = h * coefficient_h;
+
+        $(value).css({
+          'transform' : 'translate3d(' + widthPos + 'px, ' + heightPos + 'px, 0px)'
+        });
+      });
+    });
+  };
+
+  return {
+    init : init
+  };
+})();
+
 $(window).on('resize', function () {
   if( $('.feedback').length ) blur.init();
 });
 
 ;(function() {
   'use strict';
+
+  if( $('.parallax').length ) {
+    parallax.init();
+  }
+
   if( $('.feedback').length ) {
     // Blur form
     blur.init();
