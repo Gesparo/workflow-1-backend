ef65c0fa0f89297a1e7a17fe22333c195c6d0844
Melnik Maksim <shmaxer@rambler.ru>
2016-10-21 18:26:31 +0500

    Add interactive map in contacts. Changed version of the files.

--- source/js/app.js
+++ source/js/app.js
@@ -263,9 +263,106 @@ var preloader = (function () {
   };
 })();
 
+var map = (function () {
+    var createMap = function(){
+
+    var mapBlock = document.getElementById('interactiveMap');
+
+    var mapOptions = {
+      // How zoomed in you want the map to start at (always required)
+      zoom: 16,
+
+      // disable scroll
+      scrollwheel:  false,
+
+      // The latitude and longitude to center the map (always required)
+      center: new google.maps.LatLng(51.752607, 55.094532), // Orenburg
+
+      // How you would like to style the map.
+      // This is where you would paste any style found on Snazzy Maps.
+      styles: [
+        {"featureType":"landscape.natural","elementType":"geometry.fill","stylers":[{"visibility":"on"},
+          {"color":"#e0efef"}]},{"featureType":"poi","elementType":"geometry.fill","stylers":[{"visibility":"on"},
+          {"hue":"#1900ff"},{"color":"#c0e8e8"}]},{"featureType":"road","elementType":"geometry",
+          "stylers":[{"lightness":100},{"visibility":"simplified"}]},{"featureType":"road","elementType":"labels",
+          "stylers":[{"visibility":"off"}]},{"featureType":"transit.line","elementType":"geometry",
+          "stylers":[{"visibility":"on"},{"lightness":700}]},{"featureType":"water","elementType":"all",
+          "stylers":[{"color":"#61dac9"}]}]
+    };
+
+    // Create the Google Map using our element and options defined above
+    var map = new google.maps.Map(mapBlock, mapOptions);
+
+    // Let's also add a marker while we're at it
+    var marker = new google.maps.Marker({
+      position: new google.maps.LatLng(51.752607, 55.094532),
+      map: map,
+      draggable: true,
+      animation: google.maps.Animation.DROP,
+      title: 'Hi, i live here!',
+      icon: '/assets/img/map_marker.png'
+    });
+
+    marker.addListener('click', toggleBounce);
+  };
+
+  var toggleBounce = function () {
+    if (marker.getAnimation() !== null) {
+      marker.setAnimation(null);
+    } else {
+      marker.setAnimation(google.maps.Animation.BOUNCE);
+    }
+  };
+
+  var init = function () {
+    return createMap;
+  };
+
+  return {
+    init : init
+  };
+})();
+
 $(window).on('resize', function () {
   if( $('.feedback').length ) blur.init();
 });
+
+// When the window has finished loading create our google map below
+/*google.maps.event.addDomListener(window, 'load', init);
+
+function init() {
+  // Basic options for a simple Google Map
+  // For more options see: https://developers.google.com/maps/documentation/javascript/reference#MapOptions
+  var mapOptions = {
+    // How zoomed in you want the map to start at (always required)
+    zoom: 11,
+
+    // The latitude and longitude to center the map (always required)
+    center: new google.maps.LatLng(40.6700, -73.9400), // New York
+
+    // How you would like to style the map.
+    // This is where you would paste any style found on Snazzy Maps.
+    styles: [{"featureType":"landscape.natural","elementType":"geometry.fill","stylers":[{"visibility":"on"},{"color":"#e0efef"}]},{"featureType":"poi","elementType":"geometry.fill","stylers":[{"visibility":"on"},{"hue":"#1900ff"},{"color":"#c0e8e8"}]},{"featureType":"road","elementType":"geometry","stylers":[{"lightness":100},{"visibility":"simplified"}]},{"featureType":"road","elementType":"labels","stylers":[{"visibility":"off"}]},{"featureType":"transit.line","elementType":"geometry","stylers":[{"visibility":"on"},{"lightness":700}]},{"featureType":"water","elementType":"all","stylers":[{"color":"#7dcdcd"}]}]
+  };
+
+  // Get the HTML DOM element that will contain your map
+  // We are using a div with id="map" seen below in the <body>
+  var mapElement = document.getElementById('interactiveMap');
+
+  // Create the Google Map using our element and options defined above
+  var map = new google.maps.Map(mapElement, mapOptions);
+
+  // Let's also add a marker while we're at it
+  var marker = new google.maps.Marker({
+    position: new google.maps.LatLng(40.6700, -73.9400),
+    map: map,
+    title: 'Snazzy!'
+  });
+}*/
+
+if( document.getElementById('interactiveMap') ) {
+  google.maps.event.addDomListener(window, 'load', map.init());
+}
 
 $(window).on('load', function () {
   preloader.close();
@@ -273,6 +370,8 @@ $(window).on('load', function () {
 
 ;(function() {
   'use strict';
+
+
 
   //Preloader
   if( $('.preloader').length ) preloader.init();
