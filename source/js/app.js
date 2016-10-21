3fd812465fe40383f9f6d54ac7aa77722b7007fe
Melnik Maksim <shmaxer@rambler.ru>
2016-10-21 16:31:10 +0500

    Add preloader

--- source/js/app.js
+++ source/js/app.js
@@ -195,12 +195,87 @@ var parallax = (function () {
   };
 })();
 
+var preloader = (function () {
+  var imgs = [];
+  var preloaderBlock = $('.preloader');
+
+  var init = function () {
+    $.each($('*'), function () {
+      var
+          $this = $(this),
+          background = $this.css('background-image'),
+          img = $this.is('img'),
+          path = '';
+
+      if (background != 'none') {
+        path = background.replace('url("', '').replace('")', '');
+        imgs.push(path);
+      }
+
+      if (img) {
+        path = $this.attr('src');
+
+        if (path) {
+          imgs.push(path);
+        }
+      }
+    });
+
+    var percentsTotal = 1;
+
+    for (var i = 0; i < imgs.length; i++) {
+      var image = $('<img>', {
+        attr: {
+          src: imgs[i]
+        }
+      });
+
+      image.on({
+        load : function () {
+          setPercents(imgs.length, percentsTotal);
+          percentsTotal++;
+        },
+
+        error : function () {
+          percentsTotal++;
+        }
+      });
+    }
+  };
+
+  var closePreloader = function () {
+    preloaderBlock.fadeOut();
+  };
+
+  var setPercents = function(total, current) {
+    var percent = Math.ceil(current / total * 100);
+
+    if (percent >= 100) {
+      closePreloader();
+    }
+
+    $('.preloader__percents').text(percent + '%');
+  };
+
+  return {
+    init : init,
+    close : closePreloader
+  };
+})();
+
 $(window).on('resize', function () {
   if( $('.feedback').length ) blur.init();
 });
 
+$(window).on('load', function () {
+  preloader.close();
+});
+
 ;(function() {
   'use strict';
+
+  //Preloader
+  if( $('.preloader').length ) preloader.init();
 
   // Authorization block (flip)
   $('.auth-block__button').on('click', function() {
