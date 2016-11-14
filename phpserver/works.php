758328039afdeb3840361e7d03379d7b4594f1fb
Melnik Maksim <shmaxer@rambler.ru>
2016-11-14 14:48:30 +0500

    PHP: add data view for works

--- phpserver/works.php
+++ phpserver/works.php
@@ -1,7 +1,29 @@
 <?php
 session_start();
 require ('config/bootstrap.php');
+require ('core/db.php');
+
+$works = getWorks();
 
 echo $pug->render('../source/template/pages/works.pug', array(
-    'title' => 'Hello World'
+    'title' => 'Мои работы',
+    'works' => $works,
+    'lengthOfWorks' => count($works)
 ));
+
+function getWorks()
+{
+    return convertResult(DB::Query("SELECT * FROM `works`"));
+}
+
+function convertResult($data)
+{
+    $result = [];
+
+    while ($row = mysqli_fetch_assoc($data))
+    {
+        $result[] = $row;
+    }
+
+    return $result;
+}
